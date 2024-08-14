69315d895eb5daa73ba2575c3faef9fbabaabaaf
LOKITHAPOORNAJEYABALAN <164737041+LOKITHAPOORNAJEYABALAN@users.noreply.github.com>
2024-08-14 16:43:58 +0530

    Time: 5 ms (94.22%) | Memory: 59.5 MB (79.84%) - LeetSync

---
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
@@ -0,0 +1,13 @@
+// Maximum Alternating Subsequence Sum
+// Leetcode
+
+class Solution {
+    public long maxAlternatingSum(int[] A) {
+        long odd = 0, even = 0;
+        for (int a: A) {
+            even = Math.max(even, odd + a);
+            odd = even - a;
+        }
+        return even;
+    }
+}
