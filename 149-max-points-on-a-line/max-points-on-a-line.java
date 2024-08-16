d62645ab9648eaa967ecc0b4f620d84296f1604e
LOKITHAPOORNAJEYABALAN <164737041+LOKITHAPOORNAJEYABALAN@users.noreply.github.com>
2024-08-16 19:49:36 +0530

    Time: 12 ms (94.20%) | Memory: 41.1 MB (90.92%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.java
+++ 149-max-points-on-a-line/max-points-on-a-line.java
@@ -1,26 +1,33 @@
 class Solution
 {
-public int maxPoints(int[][] arr) {
-Map<Double,Integer> map=new HashMap<>();
-int n=arr.length;
-int max=0;
-double slope=0;
-for(int i=0;i<n-max-1;i++){ // i=0 to n can be decreased to i=0 to n-max-1
-int imax=0;
-for(int j=i+1;j<n;j++){
-if(arr[j][0]-arr[i][0]==0) slope=100000001;
-else if(arr[j][1]-arr[i][1]==0){
-slope=0;
-}
-else{
-slope=(double)(arr[j][1]-arr[i][1])/(double)(arr[j][0]-arr[i][0]);
-}
-map.put(slope,map.getOrDefault(slope,0)+1);
-imax=Math.max(imax,map.get(slope));
-}
-max=Math.max(max,imax);
-map.clear();
-}
-return max+1;
-}
+    public int maxPoints(int[][] points)
+    {
+        int n = points.length;
+        if( n <= 2)
+        {
+            return n;
+        }
+        int ans = 2;
+        for(int i =0 ;i<n ;i++)
+        {
+            for(int j = i+1; j<n ;j++)
+            {
+                int temp = 2;
+                for(int k =j+1;k<n ;k++)
+                {
+                    int x = (points[j][1] - points[i][1]) * (points[k][0]-points[i][0]);
+                    int  y= (points[k][1] - points[i][1]) * (points[j][0]-points[i][0]);
+                    if(x==y)
+                    {
+                        temp++;
+                    }
+                }
+                if(temp > ans)
+                {
+                    ans = temp;
+                }
+            }
+        }
+        return ans;
+    }
 }
